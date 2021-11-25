ead8a735f302bba840c8a5e10b09319c8c9f6285
Alfredo Paz Zamora <apaz0@ucol.mx>
2021-11-25 06:59:49 -0600

    Datos en mock de ejemplo

---
+++ src/mock/index.ts
@@ -0,0 +1,73 @@
+export const EnglishText = [
+    {title: 'Trivia App'},
+    {
+        questions: [
+            {id: 1, question: 'Question 1'},
+            {id: 2, question: 'Question 2'},
+            {id: 3, question: 'Question 3'},
+        ],
+        answers: [
+            {id: 1, 
+                incorrectAnswers: [
+                    'Inc 1 - 1 ',
+                    'Inc 2 - 1',
+                    'Inc 3 - 1',
+                ],
+                correctAnswers: 'Correct 1'
+            },
+            {id: 2, 
+                incorrectAnswers: [
+                    'Inc 1 - 2',
+                    'Inc 2 - 2',
+                    'Inc 3 - 2',
+                ],
+                correctAnswers: 'Correct 2'
+            },
+            {id: 3, 
+                incorrectAnswers: [
+                    'Inc 1 - 3',
+                    'Inc 2 - 3',
+                    'Inc 3 - 3',
+                ],
+                correctAnswers: 'Correct 3'
+            },
+        ]
+    }
+];
+
+export const SpanishText = [
+    {
+        questions: [
+            {id: 1, question: 'Pregunta 1'},
+            {id: 2, question: 'Pregunta 2'},
+            {id: 3, question: 'Pregunta 3'},
+        ],
+        answers: [
+            {id: 1, 
+                incorrectAnswers: [
+                    'Incorrecta 1 - 1 ',
+                    'Incorrecta 2 - 1',
+                    'Incorrecta 3 - 1',
+                ],
+                correctAnswers: 'Correcta 1'
+            },
+            {id: 2, 
+                incorrectAnswers: [
+                    'Incorrecta 1 - 2',
+                    'Incorrecta 2 - 2',
+                    'Incorrecta 3 - 2',
+                ],
+                correctAnswers: 'Correcta 2'
+            },
+            {id: 3, 
+                incorrectAnswers: [
+                    'Incorrecta 1 - 3',
+                    'Incorrecta 2 - 3',
+                    'Incorrecta 3 - 3',
+                ],
+                correctAnswers: 'Correcta 3'
+            },
+        ]
+    }
+]
+
